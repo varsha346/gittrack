b9cf920fab11519bd6cfc4b4e90b1980cad35794
Varsha Tembugade <varshatembugade346@gmail.com>
2026-07-26 11:50:43 +0530

    initial structure

---
+++ trackexp/src/main/java/com/varsha/gittrack/GittrackApplication.java
@@ -0,0 +1,13 @@
+package com.varsha.gittrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittrackApplication.class, args);
+	}
+
+}
---
+++ trackexp/src/test/java/com/varsha/gittrack/GittrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varsha.gittrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
